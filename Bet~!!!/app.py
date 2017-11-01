8b9e5be327f89992af1fc7e1bdcfeca8a84d81e0
DESKTOP-1QO3O5N\MyLaptop <nguyenthetung1806>
2017-11-01 19:05:19 +0700

    1 phần front-end

--- Bet~!!!/app.py
+++ Bet~!!!/app.py
@@ -1,37 +1,64 @@
-import mlab
 from flask import Flask, render_template, request, redirect
+import mlab
+from bson.objectid import ObjectId
 from mongoengine import *
-from faker import Faker
 
 app = Flask(__name__)
 
-#connect to mlab file
 mlab.connect()
 
-
-class Profile(Document):
+class Account(Document):
     name = StringField()
-    image = StringField()
-    description = StringField()
-    active-bet = ListField
-    completed-bet = ListField
-
-class Contract(Document):
-    contract-term = StringField;
-    contract-party-left = ListField;
-    contract-party-right = ListField;
-
-
-
-
-
-
-
+    image = FileField()
+    username = StringField()
+    password = StringField()
+    email = EmailField()
+    phone = FloatField()
 
 
 @app.route('/')
 def index():
     return render_template('homepage.html')
+
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method == "GET":
+        return render_template('signup.html')
+    elif request.method == "POST":
+        form = request.form
+        name = form['name']
+        password = form['password']
+        username = form['username']
+        image = form['image']
+        email = form['email']
+        phone = form['phone']
+        account=Account(name=name,password=password,username=username,email=email,phone=phone)
+        account.save()
+        return redirect('/login')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        form = request.form
+        username = form['username']
+        password = form['password']
+        try:
+            account = Account.objects.get(username=username)
+        except Account.DoesNotExist:
+            account = None
+        if account is None:
+            message = "Tài khoản không tồn tại"
+            return render_template('message.html', message = message)
+        else:
+            if password == account.password:
+                return render_template('profile.html', account = account)
+            else:
+                message = "Sai mật khẩu"
+                return render_template('message.html', message = message)
 
 
 if __name__ == '__main__':
