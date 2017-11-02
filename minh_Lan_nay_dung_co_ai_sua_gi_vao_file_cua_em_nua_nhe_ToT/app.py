047de13d406ea4eb57b9eda4eac3261dc9050224
DESKTOP-DVMGHEA\Veldora Tempest <minh101202@gmail.com>
2017-11-02 20:14:19 +0700

    minh

--- minh_Lan_nay_dung_co_ai_sua_gi_vao_file_cua_em_nua_nhe_ToT/app.py
+++ minh_Lan_nay_dung_co_ai_sua_gi_vao_file_cua_em_nua_nhe_ToT/app.py
@@ -14,6 +14,41 @@ class Account(Document):
     email=EmailField()
     phone=FloatField()
 
+class Keo(Document):
+    license=StringField()
+    hour1=FloatField()
+    minute1=FloatField()
+    day1=FloatField()
+    month1=FloatField()
+    year1=FloatField()
+    hour2=FloatField()
+    minute2=FloatField()
+    day2=FloatField()
+    month2=FloatField()
+    year2=FloatField()
+    join=FloatField()
+    up=StringField()
+    down=StringField()
+    guest=StringField()
+    id1=StringField()
+
+class Keo2(Document):
+    license=StringField()
+    hour1=FloatField()
+    minute1=FloatField()
+    day1=FloatField()
+    month1=FloatField()
+    year1=FloatField()
+    hour2=FloatField()
+    minute2=FloatField()
+    day2=FloatField()
+    month2=FloatField()
+    year2=FloatField()
+    join=FloatField()
+    joinlist=StringField()
+    guest=StringField()
+    id1=StringField()
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -58,15 +93,52 @@ def login():
 def lenkeo():
     if request.method=="GET":
         return render_template('lenkeo.html')
-    # elif request.method=="POST":
-    #     form=request.form
+    elif request.method=="POST":
+        form=request.form
+        license=form['license']
+        hour1=form['hour1']
+        minute1=form['minute1']
+        day1=form['day1']
+        month1=form['month1']
+        year1=form['year1']
+        hour2=form['hour2']
+        minute2=form['minute2']
+        day2=form['day2']
+        month2=form['month2']
+        year2=form['year2']
+        join=form['join']
+        up=form['up']
+        down=form['dowm']
+        guest=form['guest']
+        id1=form['id1']
+        keo=Keo(license=license,hour1=hour1,minute1=minute1,day1=day1,month1=month1,year1=year1,hour2=hour2,minute2=minute2,day2=day2,month2=month2,year2=year2,join=join,up=up,down=down,guest=guest,id1=id1)
+        keo.save()
+        return "Done!!!"
 
 @app.route('/lenkeo2', methods=['GET','POST'])
 def lenkeo2():
     if request.method=="GET":
         return render_template('lenkeo2.html')
-    # elif request.method=="POST":
-    #     form=request.form
+    elif request.method=="POST":
+        form=request.form
+        license=form['license']
+        hour1=form['hour1']
+        minute1=form['minute1']
+        day1=form['day1']
+        month1=form['month1']
+        year1=form['year1']
+        hour2=form['hour2']
+        minute2=form['minute2']
+        day2=form['day2']
+        month2=form['month2']
+        year2=form['year2']
+        join=form['join']
+        joinlist=form['joinlist']
+        guest=form['guest']
+        id1=form['id1']
+        keo2=Keo2(license=license,hour1=hour1,minute1=minute1,day1=day1,month1=month1,year1=year1,hour2=hour2,minute2=minute2,day2=day2,month2=month2,year2=year2,join=join,joinlist=joinlist,guest=guest,id1=id1)
+        keo2.save()
+        return "Done!!!"
 
 if __name__ == '__main__':
   app.run(debug=True)
